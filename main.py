74906bd7c96559e68318d08dccbf2c3ccfb2c922
RiverLawson <river.lawson@outlook.com>
2025-12-18 20:37:25 -0500

    Refactor comments for clarity in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#this imports the table from the SQLite script
 import expense_db as db
 
-
+#This prints the menu options
 def show_menu():
     print("Expense Tracker")
     print("1. Add Expense")
@@ -9,15 +10,15 @@ def show_menu():
     print("4. Delete Expense")
     print("5. Exit")
 
-
+#This is the main loop, all logic happens inside here. 
 def main():
     db.init_db()
-
+#If there is a table or a table was created, then prompt the user for input
     while True:
         show_menu()
         choice = input("Choose an option: ").strip()
 
-        # ADD EXPENSE
+        # If user chooses 1, prompt them to add information
         if choice == "1":
             amount = float(input("Enter amount: "))
             category = input("Enter category: ").strip()
@@ -27,7 +28,8 @@ def main():
             db.add_expense(amount, category, date, description)
             print("Expense added.")
 
-        # LIST EXPENSES
+        # If user chooses 2, then list expenses
+        
         elif choice == "2":
             expenses = db.list_expenses()
             if not expenses:
@@ -36,7 +38,7 @@ def main():
                 for expense in expenses:
                     print(expense)
 
-        # UPDATE EXPENSE
+        # If the user chooses 3, prompt user to input an item ID, then have user update that expense
         elif choice == "3":
             expense_id = int(input("Enter expense ID to update: "))
             amount = float(input("Enter new amount: "))
@@ -47,17 +49,17 @@ def main():
             db.update_expense(expense_id, amount, category, date, description)
             print("Expense updated.")
 
-        # DELETE EXPENSE
+        # If user chooses 4, prompt user to input an item ID, then delete that expense
         elif choice == "4":
             expense_id = int(input("Enter expense ID to delete: "))
             db.delete_expense(expense_id)
             print("Expense deleted.")
 
-        # EXIT
+        # If user chooses 5, exit
         elif choice == "5":
             print("Exiting...")
             break
-
+        #if the user doesn't type a valid input, prompt user to try again. 
         else:
             print("Invalid choice, try again.")
 
